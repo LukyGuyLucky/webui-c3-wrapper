dfdf238f2fa5f67de6c993181b91de69f6508ca8
LukyGuyLucky <111160598+LukyGuyLucky@users.noreply.github.com>
2025-12-28 13:25:49 +0800

    Add files via upload

---
+++ build_minimal_c3.bat
@@ -0,0 +1,3 @@
+c3c compile-run --wincrt=none minimal.c3 -l webui-2-static.lib -l gdi32.lib -l comctl32.lib -l ole32.lib -l user32.lib -l winmm.lib -l advapi32.lib -l shell32.lib -l libucrtd.lib -l libcmt.lib
+
+pause
